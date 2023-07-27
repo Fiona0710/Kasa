cdee30bdb9af64f7f7e8f80f1a390d9a5a9db6b9
Fiona0710 <berthou.fiona@hotmail.fr>
2023-07-27 10:45:28 +0200

    Home page with added Banner component and Cards

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,25 @@
+import "./Home.scss"
+import Banner from "../../components/Banner/Banner"
+import Card from "../../components/Card/Card";
+import data from "../../data/data.json"
 
- function Home() {
-    return (
-      <div>
-        <h1>blablablabla</h1>
-        </div>
-    )
-  }
-  export default Home
+function Home() {
   
+  return (
+    <div>
+      <Banner/>
+      <section className="card-container">
+        {data.map(item => (
+          <Card
+            key={item.id}
+            id={item.id}
+            cover={item.cover}
+            title={item.title}
+          />
+        ))}
+      </section>
+    </div>
+  );
+}
+
+export default Home;
