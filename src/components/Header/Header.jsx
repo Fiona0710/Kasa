08140a29de852d809bef69634604d1625b6547b4
Fiona0710 <berthou.fiona@hotmail.fr>
2023-07-27 10:47:38 +0200

    Creating the Header component and associated style

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,16 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom'; 
 import Logo from '../../assets/logo.png';
 import './Header.scss'
+
 export default function Header() {
   return (
-    <header className='header-container'>
+    <header className='header-block'>
       <Link to="/"> 
         <img  src={Logo} alt="logo de Kasa" />
       </Link>
-      <nav className='header-container_machin'>
-        <ul>
-            <li><Link to="/">Acceuil</Link></li> 
-            <li><Link to="/about">A propos</Link></li>
+      <nav>
+        <ul className='header-block_nav'>
+            <li><Link to="/">Accueil</Link></li> 
+            <li><Link to="/about">A Propos</Link></li>
         </ul>
       </nav>
     </header>
