e7dd18f8947f3fa2e534c5f68aa3a1e75a65cda2
Fiona0710 <berthou.fiona@hotmail.fr>
2023-07-31 19:43:27 +0200

    Added the two Collaspe components and redirected to the error page if the id does not exist

--- src/pages/Lodging/Lodging.jsx
+++ src/pages/Lodging/Lodging.jsx
@@ -1,33 +1,58 @@
-
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import './Lodging.scss';
 import data from '../../data/data.json';
 import Carousel from '../../components/Carousel/Carousel';
-import { useParams } from 'react-router-dom';
 import Host from '../../components/Host/Host';
 import TitleAndLocation from '../../components/TitleAndLocation/TitleAndLocation';
-import './Lodging.scss';
 import Tag from '../../components/Tag/Tag';
 import Rating from '../../components/Rating/Rating';
+import Collapse from '../../components/Collapse/Collapse';
+
 
 const Lodging = () => {
   const { id } = useParams(); // Extract the 'id' parameter from the URL using useParams hook
-  const lodging = data.find((item) => item.id === id); // Find the lodging data that matches the 'id'
+  const lodgingItems = data.find((item) => item.id === id); // Find the lodgingItems data that matches the 'id'
+  // The useNavigate hook allows you to programmatically navigate to different routes in your application.
+  const navigate = useNavigate();
 
-
+  useEffect(() => {
+    if (!lodgingItems) {
+      // If lodgingItems data is not found, navigate to the error page
+      navigate('*'); 
+    }
+  }, [lodgingItems, navigate]);
+  if (lodgingItems) {
   return (
-    <section>
-      {/* Render the Carousel component only if 'lodging' data is available */}
-      {lodging && <Carousel pictures={lodging.pictures} title={lodging.title} />}
-      <div className='lodgingBlock'>
-        {lodging && <Host name={lodging.host.name} picture={lodging.host.picture} />}
-        {lodging && <TitleAndLocation title={lodging.title} location={lodging.location} />}
+
+      <section>
+      <Carousel pictures={lodgingItems.pictures} title={lodgingItems.title} />
+
+      <div className='hostAndTitle'>
+        <Host name={lodgingItems.host.name} picture={lodgingItems.host.picture} />
+        <TitleAndLocation title={lodgingItems.title} location={lodgingItems.location} />
       </div>
-      <div> 
-        {lodging && <Tag tags={lodging.tags} />}
-        {lodging && <Rating rating={lodging.rating} />}
-      </div> 
+
+      <div className='TagAndRating'>
+        <Tag tags={lodgingItems.tags} />
+        <Rating rating={lodgingItems.rating} />
+      </div>
+
+      <div className='collapseLodging'>
+        <Collapse title='Description'>
+          <p>{lodgingItems.description}</p>
+        </Collapse>
+        <Collapse title='Équipements'>
+          <ul>
+          {lodgingItems.equipments.map((equipment, index) => (
+              <li key={index}>{equipment}</li>
+          ))}
+          </ul>
+        </Collapse>
+      </div>
+
     </section>
-  );
+  );}
 };
 
 export default Lodging;
