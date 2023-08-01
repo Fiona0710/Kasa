79103ef0567cc54d5b60e56b2ff1ae3599b39bc4
Fiona0710 <berthou.fiona@hotmail.fr>
2023-08-01 18:18:54 +0200

    Code refactoring

--- src/components/Rating/Rating.jsx
+++ src/components/Rating/Rating.jsx
@@ -1,34 +1,24 @@
-import './Rating.scss'
 import StarColor from '../../assets/star_color.png';
 import StarGrey from '../../assets/star_grey.png';
 
-export default function Rating ({rating}){
-  // Check if the rating is valid (between 1 and 5)
-  const isValidRating = rating >= 1 && rating <= 5;
-    return (   
-    
-     <div className="Rating">
-      {/* If the rating is valid, render colored stars and 
-      use the 'index' as the key for each star image */}
-      {isValidRating 
-        ? Array.from({ length: 5 }).map((_, index) => (
-          <img
-            key={index}
-            src={index < Math.floor(rating) ? StarColor : StarGrey}
-            alt="étoile"
-            className="Rating_star"
-          />
-        ))
-        // If the rating is not valid, render 5 gray stars
-        : Array(5).fill().map((_, index) => (
-          <img
-            key={index}
-            src={StarGrey}
-            alt="Étoile grise"
-            className="Rating_star"
-          />
-        ))
-      }   
-    </div> 
-    )
-};
+export default function Rating({ rating }) {
+  const number0fStars = parseInt(rating);
+
+  return (
+    <div>
+      {Array.from({ length: 5 }, (_, index) => (
+        <img
+          key={index}
+          src={index < number0fStars ? StarColor : StarGrey}
+          alt={index < number0fStars ? "star" : "empty-star"}
+        />
+      ))}
+    </div>
+  );
+}
+
+
+
+
+
+
